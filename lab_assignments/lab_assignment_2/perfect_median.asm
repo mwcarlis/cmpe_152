9c356957e477f41641ab3587c3e91bfcdeb92afa
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-09-24 22:37:35 -0700

    assignment_2: working partial sum.
    Wrote a partial sum function to sum 1 to N in
    x86 assembly and developing a technique for the
    perfect median problem.

--- lab_assignments/lab_assignment_2/perfect_median.asm
+++ lab_assignments/lab_assignment_2/perfect_median.asm
@@ -14,6 +14,8 @@ PrintFizz: db "%u, Fizz!",10,0
 
 PrintVal: db "%u",10,0
 
+PrintVDash: db "%u -",10,0
+
 
 SECTION .bss    ; Section containing uninitialized data
 SECTION .text   ; Section containing code
@@ -33,63 +35,138 @@ main:
 
 ; Everything before this is boilerplate; use it for all apps
 
-        mov ecx,1       ; Set Start=1
+        mov ecx,6       ; Set Start
 
-; ------  Loop 1 to 100
 loopmain:
-        mov ecx,200
-        call buzzval
 
-        mov ecx,esp
-        call buzzval
-        push 10
-        mov ecx,esp
-        call buzzval
-        mov ecx,[esp]
-        call buzzval
-        ; call isfizz     ; If it's not within [50, 59] go off diviz-a-bility.
+        ; Set partial sum ecx to push
+        push ecx
+        sub ecx,1
+        call partial_sum    ; Get lower sum 1 to ecx-1
+        mov eax,ecx         ; Move ecx to eax
+        mov ecx,[esp]       ; Get ecx from stack
+        push eax            ; Put eax/lower sum on stack
+        call subloop        ; Check all vals from ecx+1 to 10,000
+        add esp,4           ; Get eax off the stack
+        pop ecx
 
-        ; add ecx,1       ; Add Start += 1
-        ; cmp ecx,100     ; At end of loop branch statement.
 
-        ; jbe loopmain    ;
-        jmp thatsallfolks         ; END the program.
+        add ecx,1
+        cmp ecx,9999
+        jb loopmain
+        jmp thatsallfolks   ; END the program.
+
+subloop:
+        ;; get start from ecx
+        ;; go to 10,000
+        ;; get lsum from stack.
+        push eax
+        push ebx
+        push edx
+        mov eax,[esp+16]
+        mov ecx,eax
+        add ecx,1
+
+        call buzzval
+        sublooptop:
+
+                sub ecx,1
+                cmp ecx,10000
+                ; jne, jb, jbe sublooptop
+        pop edx
+        pop ebx
+        pop eax
+        ret
+
+delta_part_sum:
+        ;; Accepts unsigned int N out of ecx
+        ;; Accepts unsigned in M off the stack.
+        ;; Returns the sum of all vals from N to M in ecx
+        push eax
+        push ebx
+        push edx
+
+        call partial_sum    ; partial_sum(N)
+        push ecx            ; Save partial_sum(N)
+        mov ecx,[esp+20]    ; eax is M
+        call partial_sum    ; partial_sum(M)
+        pop eax             ; eax is partial_sum(N)
+        sub ecx,eax         ; partial_sum(M) - partial_sum(N)
+
+        pop edx
+        pop ebx
+        pop eax
+        ret
 
 partial_sum:
-        mov edx,esp
-        add edx,8
-        mov eax,[edx]
-        push esp
-        push PrintVal
-        call printf
-        add esp,4
-        pop esp
-
+        ;; Accepts one unsigned N out of ecx.
+        ;; Returns the sum of all vals from 1 to N in ecx
         push eax
-        push PrintVal
-        call printf
-        add esp,4
+        push ebx
+        push edx        ; Push register state.
+
+        mov eax,ecx     ; N eax is ecx
+        mov ebx,eax
+        add ebx,1       ; N + 1
+        mul ebx         ; Denom = (N * (N + 1))/2
+        mov ecx,eax     ; ecx is Denom
+        push 2          ; Set divisor = 2
+        call divideby   ; Do Denom / divisor
+        add esp,4       ; Pop off the divisor
+
+        pop edx
+        pop ebx
+        pop eax         ; Pop off old state.
+        ret
+
+divideby:
+        ;; Accepts N out of ecx
+        ;; Accepts unsigned M off the stack
+        ;; Returns N / M in ecx
+        push eax
+        push ebx
+        push edx
+
+        xor edx,edx     ; Clear the MSBs
+        mov eax,ecx     ; Set the LSBs: Since 100 < (2 ^ 32) - 1
+        mov ebx,[esp+16]
+        div ebx         ; Divide eax by ebx
+        mov ecx,eax     ; Move Quotent into ecx
+
+        pop edx
+        pop ebx
         pop eax
         ret
 
 isdiviz:
+        ;; Accepts N out of ecx
+        ;; Accepts 5 as static
+        ;; prints if N/5 has zero remainder.
         xor edx,edx     ; Clear the MSBs
         mov eax,ecx     ; Set the LSBs: Since 100 < (2 ^ 32) - 1
         mov ebx,5
         div ebx         ; Divide eax by 5
         cmp edx,0       ; Remainder - 0?
         jne buzzval     ; Return if Remainder != 0
-        call fiznum
+        ; call fiznum
         ret
 
 buzzval:
-        ; Print the non Fizz case.
-        ; I.e has no 5 in it's digits and is not divisible by 5.
+        ;; Print the value in ecx
+        ;;
+        push eax ; Save the registers.
+        push ebx
+        push edx
+
         push ecx
-        push PrintVal
+        push PrintVDash
         call printf
         add esp,4
-        pop ecx
+        pop ecx ; Make the registers safe again.
+
+        pop edx
+        pop ebx
+        pop eax
         ret
 
 thatsallfolks:
