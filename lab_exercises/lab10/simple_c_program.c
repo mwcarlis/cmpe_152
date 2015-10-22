80868cbfd1bd7cd96d8c753910f7ff33a094016c
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-10-22 13:27:33 -0700

    initial_commit: lab_exercises lab10, lab11
    Add lab exercises lab10, lab11 and change lab1/Makefile rules
    due to conflict with the runnable/binary naming and the .gitconfig.

---
+++ lab_exercises/lab10/simple_c_program.c
@@ -0,0 +1,29 @@
+
+
+int x;
+char lmnop[256];
+
+int main(void) {
+        int x;
+        x = 4 + 1;
+        return loyd(x);
+}
+
+int loyd(int y) {
+        int x;
+        x = 10 + y;
+        return steve(x);
+}
+
+int steve(int y) {
+        char l;
+        l = 15 + y;
+        return tom(l);
+}
+
+int tom(int weird_level) {
+        char z;
+        z = 20 + weird_level;
+        return z;
+}
+
---
+++ lab_exercises/lab11/a_c_program.c
@@ -0,0 +1,32 @@
+
+
+int x;
+char lmnop[256];
+
+int main(void) {
+        int x;
+        char p[8];
+        x = 4 + 1;
+        return loyd(x);
+}
+
+int loyd(int y) {
+        int x;
+        char p[16];
+        x = 10 + y;
+        return steve(x);
+}
+
+int steve(int y) {
+        int l;
+        char p[4];
+        l = 15 + y;
+        return tom(l);
+}
+
+int tom(int weird_level) {
+        int z;
+        z = 20 + weird_level;
+        return z;
+}
+
