d7a2ca3afe38b3282d472b76bbe3cb7218c09b73
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-11-29 21:43:33 -0800

    lab_exercise_15: update time_code.py to be dynamic
    Dynamically find files with time_code.py

--- lab_exercises/lab17/time_code.py
+++ lab_exercises/lab17/time_code.py
@@ -3,28 +3,37 @@ import timeit
 import os
 import subprocess
 
+BINARY_EXT = '.o'
+
 def binary_timer(program=''):
     if not os.path.isfile(program):
         raise Exception('{} does not exist'.format(program))
-    cmd = "./{}".format(program)
-    subprocess.check_output([cmd])
+    subprocess.check_output(["./{}".format(program)])
 
 def time_runs(program='', func_setup=''):
     if not (program and func_setup):
         raise Exception('func_setup invalid: {}'.format(func_setup))
-    setit = "from __main__ import {}".format(func_setup)
     secs = timeit.timeit(
         stmt="{}(program='{}')".format(func_setup, program.strip()),
-        setup=setit,
+        setup="from __main__ import {}".format(func_setup),
         number=1
     )
     print "Time {}: {time}".format(program, time=secs)
 
 def test_runs():
-    time_runs('main.o          ', 'binary_timer')
-    time_runs('inline_main.o   ', 'binary_timer')
-    time_runs('O3_main.o       ', 'binary_timer')
-    time_runs('O3_inline_main.o', 'binary_timer')
+    files = os.listdir(os.getcwd())
+    binaries = [runnable for runnable in files if BINARY_EXT in runnable]
+    if len(binaries) <= 0:
+        msg = "We didn't find any executables to run. <>{}"
+        raise Exception(msg.format(BINARY_EXT))
+    width_fmat = '{}'.format(len(max(binaries, key=len)))
+    row = '{:' + width_fmat + '}'
+    for binary in binaries:
+        time_runs(
+            row.format(binary),
+            'binary_timer'
+        )
+
 
 if __name__ == "__main__":
     test_runs()
